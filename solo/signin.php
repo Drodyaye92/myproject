4f02326c0d92aa1dfdc52567c488c09b87caeaf7
Drodyaye92 <aichanana24@gmail.com>
2021-01-29 19:20:02 +0000

    mon troisieme commit

--- solo/signin.php
+++ solo/signin.php
@@ -7,13 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-   // header("location: login.php");
-   require_once 'signtraitement.php';
-    ?>
+   
 <div class="design">
 <h1 id="middle"> SE CONNECTER</h1>
-   <form action="traitement.php" method="POST">
+   <form action="signtraitement.php" method="POST">
    <h2 class="para"> Email
    <input type="email" class="textinput" name="email" placeholder="E-mail" required>
    </h2>
@@ -21,7 +18,7 @@
    <input type="password" class="textinput" name="password" placeholder="Password" required></h2>
     <button type="submit" class="button" name="connexion" value="submit">connexion</button>
     </form> 
-     <a href="signin.php" class="change">Creer un compte</a>
+     <a href="login.php" class="change">Creer un compte</a>
     </div>
 </body>
 </html>
--- solo/signtraitement.php
+++ solo/signtraitement.php
@@ -1,18 +1,21 @@
 <?php 
+require_once "connexion.php";
+$email=$_POST['email'];
+$password=$_POST['password'];
 if(isset($_POST['connexion'])){
        
        extract($_POST);
-       if(!empty($email) && !empty($pass)){
+       if(!empty($email) && !empty($password)){
 $q = $db->prepare("SELECT * FROM utilisateur WHERE email=:email");
 $q ->execute(['email' =>$email]);
 $result = $q->fetch();
-var_dump($result);
+
 }
 if($result == true){
     //le compte existe bien
-    $hashpassword= $result['$password'];
+    $hashpassword= $result['pass'];
     }
-    if(password_verify($pass,result ['password'])){
+    if(password_verify($password,$_POST['password'])){
     echo "le mot de passe est bon,connection en cours";
        }
     }
