8a39f37bc203f6f3be847db045be05dad89d9777
Drodyaye92 <aichanana24@gmail.com>
2021-02-02 12:36:21 +0000

    commit de boucles

--- solo/signtraitement.php
+++ solo/signtraitement.php
@@ -6,26 +6,48 @@ if(isset($_POST['connexion'])){
        
        extract($_POST);
 
-      if(!empty($email) && !empty($password)){
-$q = $db->prepare("SELECT * FROM utilisateur WHERE email=:email");
-$q ->execute(['email' =>$email]);
-$result = $q->fetch();
+      if(!empty($email) && !empty($password))
+{ 
+         $q = $db->prepare("SELECT * FROM utilisateur WHERE email=:email");
+         $q ->execute(['email' =>$email]);
+         $result = $q->fetch();
+         if($result == 1){
 
-if($result == true){
+         
+if(password_verify($password,$_POST['password']))
+if($result == true){$hashpassword= $result['pass'];
+  
 
- //le compte existe bien
-
-   $hashpassword= $result['pass'];
-   if(password_verify($password,$_POST['password']))  
-      {
-        echo "le mot de passe est bon,connection en cours";
-      }
-
-   else{
-        echo "le mot de passe  est incorrecte";
+   
+   {
+       if(filter_var($email, FILTER_VALIDATE_EMAIL))
+       {
+           
+               if(password_verify($password, $data['password']))
+                   {
+                      $_SESSION['user'] = $data['email'];
+                       header('Location: http://localhost/myproject/index.php');
+                      die();
+                   }
+              else
+               { 
+                  header('Location: index.php?login_err=password'); die();
+               }
        }
-
-    }
+       else
+          { 
+             header('Location: signin.php?login_err=email'); die(); 
+          }
+   }
+   else
+   { header('Location: signin.php?login_err=already'); die(); 
    }
 }
-   ?>
+}
+}
+}
+
+?>
+ 
+
+ 
